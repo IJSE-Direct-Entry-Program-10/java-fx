88d4f3fdaeee35d1593161ad95deebe10053449e
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-02-15 11:39:36 +0530

    Finalize the design of the ListViewExerciseScene

--- 02-java-fx-fundamentals/java-fx-last/src/main/java/lk/ijse/dep10/last/controller/ListViewExerciseSceneController.java
+++ 02-java-fx-fundamentals/java-fx-last/src/main/java/lk/ijse/dep10/last/controller/ListViewExerciseSceneController.java
@@ -1,4 +1,125 @@
 package lk.ijse.dep10.last.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyEvent;
+
 public class ListViewExerciseSceneController {
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnForward;
+
+    @FXML
+    private Button btnNewStudent;
+
+    @FXML
+    private Button btnRemove;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private ToggleGroup gender;
+
+    @FXML
+    private ListView<?> lstContacts;
+
+    @FXML
+    private ListView<?> lstModules;
+
+    @FXML
+    private ListView<?> lstSelectedModules;
+
+    @FXML
+    private ListView<?> lstStudents;
+
+    @FXML
+    private RadioButton rdoFemale;
+
+    @FXML
+    private RadioButton rdoMale;
+
+    @FXML
+    private TextField txtContact;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBackOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnForwardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewStudentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRemoveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void lstContactsOnKeyReleased(KeyEvent event) {
+
+    }
+
+    @FXML
+    void lstModulesOnKeyReleased(KeyEvent event) {
+
+    }
+
+    @FXML
+    void lstSelectedModulesOnKeyReleased(KeyEvent event) {
+
+    }
+
+    @FXML
+    void lstStudentsOnKeyReleased(KeyEvent event) {
+
+    }
+
+    @FXML
+    void txtContactOnAction(ActionEvent event) {
+
+    }
+
 }
