d40a1aad60392cc71a959a1b2e04893e5f6764c4
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-02-23 23:22:34 +0530

    Add javafx-concurrency

--- 02-java-fx-fundamentals/java-fx-last/src/main/java/lk/ijse/dep10/last/controller/ComboBoxSceneController.java
+++ 02-java-fx-fundamentals/java-fx-last/src/main/java/lk/ijse/dep10/last/controller/ComboBoxSceneController.java
@@ -3,10 +3,8 @@ package lk.ijse.dep10.last.controller;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.CheckBoxListCell;
 
 public class ComboBoxSceneController {
 
@@ -23,6 +21,7 @@ public class ComboBoxSceneController {
     private TextField txtInput;
 
     public void initialize(){
+
         ObservableList<String> itemList = cmbSomething.getItems();
         itemList.addAll("Kasun", "Nuwan", "Ruwan");
         cmbSomething.getSelectionModel().selectedItemProperty().addListener((value, previous, current) -> {
